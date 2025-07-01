dd193b0841798ab1d8799d4e33112d1ecf1575e6
Anil <anilbca1996@gmail.com>
2025-07-01 17:59:47 +0530

    added sidebar

--- src/App.js
+++ src/App.js
@@ -1,21 +1,48 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
 import Header from './Header';
 import About from './About';
 import Contact from './Contact';
 import Home from './Home';
 import Dashboard from './Dashboard';
 
+// You can create placeholder components if you don't have them yet
+function AddSociety() {
+  return <h2>Add Society Page</h2>;
+}
+
+function AddHouse() {
+  return <h2>Add House Page</h2>;
+}
+
 function App() {
   return (
     <BrowserRouter>
       <Header />
-      <div className="container mt-3">
-        <Routes>
-           <Route path="/dashboard" element={<Dashboard/>} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
+      <div className="container mt-3 d-flex">
+        {/* Sidebar */}
+        <div className="sidebar p-3 border-end" style={{ width: "200px" }}>
+          <h5>Menu</h5>
+          <ul className="list-unstyled">
+            <li>
+              <Link to="/add-society">Add Society</Link>
+            </li>
+            <li>
+              <Link to="/add-house">Add House</Link>
+            </li>
+          </ul>
+        </div>
+
+        {/* Main Content */}
+        <div className="flex-grow-1 p-3">
+          <Routes>
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/add-society" element={<AddSociety />} />
+            <Route path="/add-house" element={<AddHouse />} />
+          </Routes>
+        </div>
       </div>
     </BrowserRouter>
   );
